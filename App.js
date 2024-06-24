378a4dd5fe05e46cc508237e9faf9ac453907891
AshaPreethi <ashapreethi@ezinnovation.com>
2024-06-24 14:20:39 +0530

    ep-1

---
+++ App.js
@@ -0,0 +1,16 @@
+const parent = React.createElement("div");
+
+const heading = React.createElement(
+  "div",
+  { id: "parent" },[
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "h1 Tag"),
+    React.createElement("h2", {}, "h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "h1 Tag"),
+    React.createElement("h2", {}, "h2 tag"),
+  ])]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
